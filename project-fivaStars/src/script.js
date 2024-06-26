c0fe404e074290f601e443187496ea23686c82d4
v1nny:) <vinicioolivindo92@gmail.com>
2024-06-26 17:25:52 -0300

    att

--- project-fivaStars/src/script.js
+++ project-fivaStars/src/script.js
@@ -1,1 +1,12 @@
-// write your JavaScript here
+const todasEstrelas = document.querySelectorAll("li")
+const unicaEstrela = document.querySelector(".unicStar")
+
+
+todasEstrelas.forEach(li => {
+    li.addEventListener("click", function() {
+        li.classList.add("estrelaClicada")
+        todasEstrelas.forEach(star => {
+            if()
+        })
+    })
+})
